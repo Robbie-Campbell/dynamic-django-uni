e41cfdb2db6891c0ae465a40ada920a643e8eca1
robbie <robbielcampbell@hotmail.com>
2021-01-08 11:11:32 +0000

    hidden secret key

---
+++ core/secrets.py
@@ -0,0 +1,1 @@
+secret = '@mhq9@2_v*3rlp!krppeq&pnj7b%clj(ft4)xq%oa!4p!!v0@8'
--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+from secrets import secret
 """
 Django settings for core project.
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@mhq9@2_v*3rlp!krppeq&pnj7b%clj(ft4)xq%oa!4p!!v0@8'
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
