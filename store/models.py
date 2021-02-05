1836902e63b492aa65b606ce5855450e0bd4e343
robbie <robbielcampbell@hotmail.com>
2021-02-05 11:25:29 +0000

    :tada: lesson 5 done :tada:

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -16,12 +17,18 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
     slug = models.SlugField(max_length=20, unique=True)
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', default='images/default.png')
     author = models.CharField(max_length=255)
     desc = models.TextField(blank=True)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name_plural = "Products"
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("store:product_detail", args=[self.slug])
