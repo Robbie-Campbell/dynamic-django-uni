63dd4f5f5d19aed57ea3f454becd0094ed3f2df3
robbie <robbielcampbell@hotmail.com>
2021-02-14 14:09:29 +0000

    :shipit: testing etc. added :shipit:

--- store/admin.py
+++ store/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 
 from .models import Category, Product
 
-admin.site.register(Category)
-
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['title']}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['name']}
--- store/urls.py
+++ store/urls.py
@@ -5,8 +5,7 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('products/', views.products, name='products'),
+    path('', views.product_list, name='product_list'),
     path('product/<slug:slug>', views.product_detail, name='product_detail'),
-    path('category/<slug:slug>', views.category_detail, name='category_detail'),
+    path('category/<slug:slug>', views.category_list, name='category_list'),
 ]
