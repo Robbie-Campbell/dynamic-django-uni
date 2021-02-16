2580ac17bd4bcf7bf2c0219112a4a5daaedf621f
robbie <robbielcampbell@hotmail.com>
2021-02-16 14:42:01 +0000

    :construction: work in progress :construction:

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,5 @@
+from store.models import Product
+from decimal import Decimal
 
 
 class Basket():
@@ -14,4 +16,34 @@ class Basket():
         if product_id not in self.basket:
             self.basket[product_id] = {'price': str(product.price), 'qty': int(product_qty)}
 
+        self.save()
+
+    def __iter__(self):
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+    def __len__(self):
+        return sum(item['qty'] for item in self.basket.values())
+    
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+    def delete(self, product):
+
+        product_id = product
+
+        if product_id is self.basket:
+            del self.basket[product_id]
+            self.save()
+
+    def save(self):
         self.session.modified = True
